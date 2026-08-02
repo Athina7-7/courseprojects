300339a4a88b44314efa7b99d610cbf44bd3be4b
Athina7-7 <cappellettiathina@gmail.com>
2026-08-02 16:33:27 -0500

    Add book management features and update views
    - Introduced Book model and data structure for managing books.
    - Implemented routes for displaying a list of books and individual book details.
    - Created views for listing books and showing book details.
    - Enhanced navigation with a link to the books section.

--- fullstack/src/controllers/HomeController.ts
+++ fullstack/src/controllers/HomeController.ts
@@ -1,4 +1,6 @@
 import type { Request, Response } from 'express';
+import { books } from '../data/Books.js';
+import { Book } from '../models/Book.js';
 
 export class HomeController {
   static index(req: Request, res: Response): void {
@@ -16,6 +18,18 @@ export class HomeController {
     res.render('home/about', { viewData: viewData });
 
   }
+  static Main_Point(req: Request, res: any) {
+    const viewData: any = {};
+     viewData["books"] = books;
+    res.render('home/books', viewData);
+  }
+
+  static show(req: any, res: any){
+    const book = Book.findById(books, parseInt(req.params.id));
+
+    res.render('home/show', { book: book })
+  }
+
   static contacts(req: Request, res: Response): void {
     const viewData: { [key: string]: any } = {};
     viewData["title"] = "Contact";
